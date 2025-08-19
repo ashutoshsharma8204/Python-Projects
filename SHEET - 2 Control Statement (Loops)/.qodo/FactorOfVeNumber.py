5723a9e7efe31afd81f692d02d617aaa7ffa3722
Ashutosh sharma <ashutoshsharma8204@gmail.com>
2025-08-19 23:38:40 +0530

    Add Projects

---
+++ SHEET - 2 Control Statement (Loops)/.qodo/FactorOfVeNumber.py
@@ -0,0 +1,7 @@
+# Print all factors of a given positive number
+num = int(input("Enter a positive number: "))
+i = 1
+while ( i <= num ):
+        if num % i == 0:
+            print(i)
+        i += 1
